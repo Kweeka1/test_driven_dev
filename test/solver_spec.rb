3d24aa50e4374dcbd3e171f36b482c65b701c683
kweeka <mtalbi29@yahoo.com>
2023-02-10 16:09:21 +0100

    Add test for fizzbuzz method

--- test/solver_spec.rb
+++ test/solver_spec.rb
@@ -1,8 +1,18 @@
 describe Solver do
+  let(:solver) { Solver.new }
+
   it 'should factorize num' do
-    expect { factorize(1) }.to raise_error ArgumentError
+    expect { solver.factorize(1) }.to raise_error ArgumentError
   end
+
   it 'reverses a string' do
-    expect(reverse('hello')).to eq('olleh')
+    expect(solver.reverse('hello')).to eq('olleh')
+  end
+
+  it 'should solve fizzbuzz challenge' do
+    expect(solver.fizzbuzz(10)).to match '1 2 fizz 4 buzz fizz 7 8 fizz buzz'
+    expect(solver.fizzbuzz(15)).to match '1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz'
+    expect(solver.fizzbuzz(18)).to match '1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz'
+    expect(solver.fizzbuzz(3)).to match '1 2 fizz'
   end
 end
